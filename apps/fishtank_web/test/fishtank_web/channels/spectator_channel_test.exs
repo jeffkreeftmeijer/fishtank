875d16b9af5c334747c49a01d8c89ae3d5113e97
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-23 09:18:39 +0100

    Set up SpectatorChannel
    The generated `SpectatorChannel` includes stubs to handle multiple
    rooms, pings and authorization. Since the spectator channel is in a
    single room that's publicly available, this code is unneeded.
    
    A minimal implementation of the `SpectatorChannel` only has a function
    that handles joins.
    
    ``` elixir
    defmodule FishtankWeb.SpectatorChannel do
      use FishtankWeb, :channel
    
      def join("fishtank", _message, socket) do
        {:ok, socket}
      end
    end
    ```
    
    The `SpectatorChannelTest` has a function that checks if joining the
    channel returns a `Phoenix.Socket`:
    
    ``` elixir
    defmodule FishtankWeb.SpectatorChannelTest do
      setup do
        {:ok, _, socket} =
          FishtankWeb.UserSocket
          |> socket()
          |> subscribe_and_join(FishtankWeb.SpectatorChannel, "fishtank")
    
        %{socket: socket}
      end
    
      test "broadcasts are pushed to the client", %{socket: socket} do
        broadcast_from!(socket, "broadcast", %{"some" => "data"})
        assert_push "broadcast", %{"some" => "data"}
      end
    
      test "joins the channel", %{socket: socket} do
        assert %Phoenix.Socket{
                 joined: true,
                 channel: FishtankWeb.SpectatorChannel,
                 handler: FishtankWeb.UserSocket
               } = socket
      end
    end
    ```

--- apps/fishtank_web/test/fishtank_web/channels/spectator_channel_test.exs
+++ apps/fishtank_web/test/fishtank_web/channels/spectator_channel_test.exs
@@ -4,24 +4,22 @@ defmodule FishtankWeb.SpectatorChannelTest do
   setup do
     {:ok, _, socket} =
       FishtankWeb.UserSocket
-      |> socket("user_id", %{some: :assign})
-      |> subscribe_and_join(FishtankWeb.SpectatorChannel, "spectator:lobby")
+      |> socket()
+      |> subscribe_and_join(FishtankWeb.SpectatorChannel, "fishtank")
 
     %{socket: socket}
   end
 
-  test "ping replies with status ok", %{socket: socket} do
-    ref = push socket, "ping", %{"hello" => "there"}
-    assert_reply ref, :ok, %{"hello" => "there"}
-  end
-
-  test "shout broadcasts to spectator:lobby", %{socket: socket} do
-    push socket, "shout", %{"hello" => "all"}
-    assert_broadcast "shout", %{"hello" => "all"}
-  end
-
   test "broadcasts are pushed to the client", %{socket: socket} do
-    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    broadcast_from!(socket, "broadcast", %{"some" => "data"})
     assert_push "broadcast", %{"some" => "data"}
+  end
+
+  test "joins the channel", %{socket: socket} do
+    assert %Phoenix.Socket{
+             joined: true,
+             channel: FishtankWeb.SpectatorChannel,
+             handler: FishtankWeb.UserSocket
+           } = socket
   end
 end
