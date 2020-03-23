beb5236c247aa938b47f09e19495cf300d0f0845
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-23 13:37:54 +0100

    Fetch state in app.js, print state to console every second

--- apps/fishtank_web/assets/js/app.js
+++ apps/fishtank_web/assets/js/app.js
@@ -2,3 +2,21 @@ import "../css/app.css"
 import "phoenix_html"
 import {Socket} from "phoenix"
 import {Fishtank} from "./fishtank"
+
+let socket = new Socket("/socket", {params: {token: window.userToken}})
+socket.connect()
+
+let fishtank = new Fishtank;
+
+let channel = socket.channel("fishtank", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+channel.on("update", payload => {
+  fishtank.update(payload)
+})
+
+setInterval(() => { console.log(fishtank.state) }, 1000)
+
+export default socket
--- apps/fishtank_web/assets/js/fishtank.ts
+++ apps/fishtank_web/assets/js/fishtank.ts
@@ -1,2 +1,7 @@
 export class Fishtank {
+  state: object
+
+  update(state) {
+    this.state = state;
+  }
 }
