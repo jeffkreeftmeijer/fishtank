8ac28349bf366a59596753f72dc4e5bad91465c0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-01 21:00:55 +0200

    Add Vector.add/2

--- apps/vector/lib/vector.ex
+++ apps/vector/lib/vector.ex
@@ -1,2 +1,17 @@
 defmodule Vector do
+  @type t :: {float, float}
+
+  @doc ~S"""
+  Adds two vectors.
+
+  ## Example
+
+      iex> Vector.add({1.0, 2.0}, {3.0, 4.0})
+      {4.0, 6.0}
+
+  """
+  @spec add(t, t) :: t
+  def add({x_one, y_one}, {x_two, y_two}) do
+    {x_one + x_two, y_one + y_two}
+  end
 end
