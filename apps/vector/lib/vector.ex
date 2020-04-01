d4475cea5661e946eeff8eeca0383c6b921e6d60
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-01 22:49:59 +0200

    Allow vectors with dimensions other than 2

--- apps/vector/lib/vector.ex
+++ apps/vector/lib/vector.ex
@@ -1,31 +1,47 @@
 defmodule Vector do
-  @type t :: [float, float]
+  @type t :: [float]
 
   @doc ~S"""
   Adds two vectors.
 
-  ## Example
+  ## Examples
 
-      iex> Vector.add([1.0, 2.0], [3.0, 4.0])
-      [4.0, 6.0]
+      iex> Vector.add([1.0], [-0.6])
+      [0.4]
+      iex> Vector.add([1.0, -2.0], [-0.6, 0.5])
+      [0.4, -1.5]
+      iex> Vector.add([1.0, -2.0, 3.0], [-0.6, 0.5, -0.4])
+      [0.4, -1.5, 2.6]
 
   """
   @spec add(t, t) :: t
-  def add([x_one, y_one], [x_two, y_two]) do
-    [x_one + x_two, y_one + y_two]
+  def add(one, two) do
+    one
+    |> Enum.with_index()
+    |> Enum.map(fn {value, index} ->
+      value + Enum.at(two, index)
+    end)
   end
 
   @doc ~S"""
   Subtracts two vectors.
 
-  ## Example
+  ## Examples
 
-      iex> Vector.subtract([3.0, 4.0], [1.0, 2.0])
-      [2.0, 2.0]
+      iex> Vector.subtract([1.0], [-0.6])
+      [1.6]
+      iex> Vector.subtract([1.0, -2.0], [-0.6, 0.5])
+      [1.6, -2.5]
+      iex> Vector.subtract([1.0, -2.0, 3.0], [-0.6, 0.5, -0.4])
+      [1.6, -2.5, 3.4]
 
   """
   @spec subtract(t, t) :: t
-  def subtract([x_one, y_one], [x_two, y_two]) do
-    [x_one - x_two, y_one - y_two]
+  def subtract(one, two) do
+    one
+    |> Enum.with_index()
+    |> Enum.map(fn {value, index} ->
+      value - Enum.at(two, index)
+    end)
   end
 end
