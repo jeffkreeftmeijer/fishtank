79414253453e02a5b9b9ca784cbc61701266c277
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-01 20:45:33 +0200

    $ mix new apps/vector

---
+++ apps/vector/lib/vector.ex
@@ -0,0 +1,18 @@
+defmodule Vector do
+  @moduledoc """
+  Documentation for `Vector`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vector.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
