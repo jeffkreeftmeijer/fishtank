e272cd65d86f95315b4a633a44c0ad25a6ccdef6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-01 21:20:53 +0200

    Add Vector.subtract/2

--- apps/vector/lib/vector.ex
+++ apps/vector/lib/vector.ex
@@ -14,4 +14,18 @@ defmodule Vector do
   def add({x_one, y_one}, {x_two, y_two}) do
     {x_one + x_two, y_one + y_two}
   end
+
+  @doc ~S"""
+  Subtracts two vectors.
+
+  ## Example
+
+      iex> Vector.subtract({3.0, 4.0}, {1.0, 2.0})
+      {2.0, 2.0}
+
+  """
+  @spec subtract(t, t) :: t
+  def subtract({x_one, y_one}, {x_two, y_two}) do
+    {x_one - x_two, y_one - y_two}
+  end
 end
