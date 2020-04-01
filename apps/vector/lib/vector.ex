6c3b2baebe50db014f9ec26d7692e4c80a78a464
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-01 20:50:32 +0200

    Remove boilerplate from Vector
        $ sed -i '' 14d apps/vector/mix.exs
        $ sed -i '' 20d apps/vector/mix.exs
        $ sed -i '' 22,23d apps/vector/mix.exs
    
        $ sed -i '' 4,7d apps/vector/test/vector_test.exs
        $ sed -i '' 2,17d apps/vector/lib/vector.ex

--- apps/vector/lib/vector.ex
+++ apps/vector/lib/vector.ex
@@ -1,18 +1,2 @@
 defmodule Vector do
-  @moduledoc """
-  Documentation for `Vector`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Vector.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
