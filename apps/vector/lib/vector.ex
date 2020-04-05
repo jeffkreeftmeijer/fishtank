d9912577d6afc0fefc2e3e9711ade5dd22970076
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-05 11:00:40 +0200

    Add Vector.multiply/2 and .divide/2

--- apps/vector/lib/vector.ex
+++ apps/vector/lib/vector.ex
@@ -37,6 +37,42 @@ defmodule Vector do
     zip(one, two, [], &Kernel.-/2)
   end
 
+  @doc ~S"""
+  Multiplies a vector.
+
+  ## Examples
+
+      iex> Vector.multiply([1.0], 1.0)
+      [1.0]
+      iex> Vector.multiply([1.0, -2.0], -2.0)
+      [-2.0, 4.0]
+      iex> Vector.multiply([1.0, -2.0, 3.0], 3.0)
+      [3.0, -6.0, 9.0]
+
+  """
+  @spec multiply(t, float) :: t
+  def multiply(vector, multiplier) do
+    Enum.map(vector, &(&1 * multiplier))
+  end
+
+  @doc ~S"""
+  Divides a vector.
+
+  ## Examples
+
+      iex> Vector.divide([1.0], 1.0)
+      [1.0]
+      iex> Vector.divide([1.0, -2.0], -2.0)
+      [-0.5, 1.0]
+      iex> Vector.divide([1.0, -2.0, 3.0], 4.0)
+      [0.25, -0.5, 0.75]
+
+  """
+  @spec divide(t, float) :: t
+  def divide(vector, divider) do
+    Enum.map(vector, &(&1 / divider))
+  end
+
   defp zip([one | one_tail], [two | two_tail], acc, fun) do
     zip(one_tail, two_tail, [fun.(one, two) | acc], fun)
   end
