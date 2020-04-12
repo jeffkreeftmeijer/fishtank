2a6025e87eedc6dbefef39416ec37504de5988dc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-12 02:20:01 +0200

    Add regression test for .update/1 LocationTest

--- apps/fishtank/test/fishtank/entity/location_test.exs
+++ apps/fishtank/test/fishtank/entity/location_test.exs
@@ -1,4 +1,25 @@
 defmodule Fishtank.Entity.LocationTest do
   use ExUnit.Case
+  use ExUnitProperties
   doctest Fishtank.Entity.Location
+  alias Fishtank.{Entity, Entity.Location}
+
+  property "update/1 adds the Entity's velocity to its location" do
+    check all(%Entity{location: [lx, ly], velocity: [vx, vy]} = entity <- entity()) do
+      %Entity{location: location} = Location.update(entity)
+      assert location == [lx + vx, ly + vy]
+    end
+  end
+
+  defp entity() do
+    gen all(location <- vector(), velocity <- vector()) do
+      %Entity{location: location, velocity: velocity}
+    end
+  end
+
+  defp vector() do
+    gen all(x <- float(), y <- float()) do
+      [x, y]
+    end
+  end
 end
